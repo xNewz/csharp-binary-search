8de0017bb7f845a413d3dedbf21d1019113b3e44
xNewz <pargornruasijan@gmail.com>
2022-04-19 23:13:05 +0700

    add: create variables

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace csharp_binary_search
     {
         static void find_num(int[] arr, int target)
         {
-            
+            int first = 0, last = arr.Length - 1, mid = 0;
         }
         static void Main(string[] args)
         {
