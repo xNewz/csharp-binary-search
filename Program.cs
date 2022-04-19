79563059e9f7f36d5ce30d92f089e0f297d9cdc6
xNewz <pargornruasijan@gmail.com>
2022-04-19 23:15:16 +0700

    add: create while loop

--- Program.cs
+++ Program.cs
@@ -7,6 +7,18 @@ namespace csharp_binary_search
         static void find_num(int[] arr, int target)
         {
             int first = 0, last = arr.Length - 1, mid = 0;
+            
+            while (first <= last)
+            {
+                mid = (first + last) / 2;
+                if (target > arr[mid])
+                    first = mid + 1;
+                else
+                    if (target < arr[mid])
+                        last = mid - 1;
+                    else
+                        first = last + 1;
+            }
         }
         static void Main(string[] args)
         {
