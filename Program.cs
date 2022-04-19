993fae1a27d34f14883b64bde592c9ebc8429cd9
xNewz <pargornruasijan@gmail.com>
2022-04-19 23:11:25 +0700

    add: create project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharp_binary_search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
